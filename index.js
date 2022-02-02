3ba28f70cd7548d2fcc1068cb30bb42af28913a3
arjungarg07 <arjun.garg.cse01@gmail.com>
2022-02-02 16:28:10 +0530

    chore: change seed Urls

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ async function saveData(data) {
     console.log(res2);
 }
 
-const stackOverFlowScrapper = new Scraper({pageLimitForSeedUrls: 10, concurrenyLimit: 5, delay: 200 });
+const stackOverFlowScrapper = new Scraper({pageLimitForSeedUrls: 100, concurrenyLimit: 5, delay: 200 });
 stackOverFlowScrapper.start();
 
 // setInterval(()=>{
